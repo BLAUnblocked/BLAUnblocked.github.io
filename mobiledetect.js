e04f7a60f5e66f833c963fc6183cc8f9fb9e84e3
Daniel Reynolds <50958520+camelCaseCo@users.noreply.github.com>
2019-10-14 10:51:28 -0400

    Create mobiledetect.js

---
+++ mobiledetect.js
@@ -0,0 +1,4 @@
+if (/Android|Mobile|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+    window.location = '/m'
+}
+window.alert(navigation.userAgent);

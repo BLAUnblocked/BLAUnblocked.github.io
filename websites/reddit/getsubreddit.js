7404b2f74d2033c4af00380c0f1c15f49b1f8295
Daniel Reynolds <50958520+camelCaseCo@users.noreply.github.com>
2019-10-14 19:20:55 -0400

    Update getsubreddit.js

--- websites/reddit/getsubreddit.js
+++ websites/reddit/getsubreddit.js
@@ -4,4 +4,5 @@ let subredditDisplay = document.getElementById('subreddit');
 function loadSubreddit() {
 	window.alert(subredditDisplay.attributes[0].name + ', ' + subredditDisplay.attributes[1].name + ', ' + subredditDisplay.attributes[2].name + ', ' + subredditDisplay.attributes[3].name)
 	subredditDisplay.attributes[2] = subredditInput.innerHTML;
+	window.alert('ree');
 }
